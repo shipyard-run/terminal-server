210946a0d3cd09039da2f5314fce87c95b510b7d
Erik Veld <eveld@hashicorp.com>
2019-12-11 16:29:36 +0100

    initial commit

---
+++ go.mod
@@ -0,0 +1,11 @@
+module github.com/shipyard-run/tty
+
+go 1.13
+
+require (
+	github.com/gorilla/handlers v1.4.2
+	github.com/gorilla/mux v1.7.3
+	github.com/gorilla/websocket v1.4.1
+	github.com/kr/pty v1.1.8
+	golang.org/x/sys v0.0.0-20191210023423-ac6580df4449 // indirect
+)
---
+++ main.go
@@ -0,0 +1,56 @@
+package main
+
+import (
+	"fmt"
+	"log"
+	"net/http"
+
+	"github.com/shipyard-run/tty/server"
+
+	"github.com/gorilla/handlers"
+	"github.com/gorilla/mux"
+	// "github.com/sevlyar/go-daemon"
+)
+
+func main() {
+	// if cfg.Daemon {
+	// 	ctx := &daemon.Context{
+	// 		PidFileName: "/var/run/shipyard.pid",
+	// 		PidFilePerm: 0644,
+	// 		LogFileName: "/var/log/shipyard.log",
+	// 		LogFilePerm: 0640,
+	// 		WorkDir:     "/",
+	// 		Umask:       027,
+	// 		Args:        []string{"[shipyard]"},
+	// 	}
+
+	// 	d, err := ctx.Reborn()
+	// 	if err != nil {
+	// 		log.Fatal(err)
+	// 	}
+	// 	if d != nil {
+	// 		return
+	// 	}
+	// 	defer ctx.Release()
+	// }
+
+	r := mux.NewRouter()
+	server.HandleTerminal(r.PathPrefix("/terminal").Subrouter())
+
+	r.HandleFunc("/", func(w http.ResponseWriter, r *http.Request) {
+		w.WriteHeader(http.StatusOK)
+		w.Write([]byte("OK"))
+	})
+	r.Methods("OPTIONS").HandlerFunc(func(w http.ResponseWriter, r *http.Request) {
+		w.WriteHeader(http.StatusOK)
+	})
+
+	headersOk := handlers.AllowedHeaders([]string{"Content-Type", "Origin", "Accept", "Token"})
+	originsOk := handlers.AllowedOrigins([]string{"*"})
+	methodsOk := handlers.AllowedMethods([]string{"GET", "HEAD", "POST", "PUT", "DELETE", "OPTIONS"})
+	corsHandler := handlers.CORS(headersOk, originsOk, methodsOk)
+
+	address := fmt.Sprintf("%s:%d", "0.0.0.0", 27950)
+	log.Printf("Listening on http://%s", address)
+	log.Fatal(http.ListenAndServe(address, corsHandler(r)))
+}
